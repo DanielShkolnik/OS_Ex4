9639f01c265a1b6b2156fc6559a9f73072bfd160
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2020-06-13 13:42:23 +0300

    new

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -12,6 +12,14 @@ public:
     bool is_free;
     MallocMetadataNode* next;
     MallocMetadataNode* prev;
+
+    MallocMetadataNode(void* allocatedBlockPtr, size_t size):allocatedBlockPtr(allocatedBlockPtr),size(size),
+                        is_free(false),next(nullptr),prev(nullptr){};
+
+    MallocMetadataNode(const MallocMetadataNode &mallocMetadataNode) = delete;
+    MallocMetadataNode &operator=(const MallocMetadataNode &mallocMetadataNode) = delete;
+
+    ~MallocMetadataNode() = default;
 };
 
 class MallocMetadataNodeList {
@@ -24,11 +32,54 @@ public:
     MallocMetadataNode* head;
     MallocMetadataNode* tail;
 
+    MallocMetadataNodeList():numOfFreeBlocks(0),sizeOfFreeBlocks(0),numOfUsedBlocks(0),sizeOfUsedBlocks(0),
+                            sizeOfSingleMetadataBlock(0),head(nullptr),tail(nullptr){};
+
+    MallocMetadataNodeList(const MallocMetadataNodeList &mallocMetadataNodeList) = delete;
+    MallocMetadataNodeList &operator=(const MallocMetadataNodeList &mallocMetadataNodeList) = delete;
+
+    ~MallocMetadataNodeList(){
+        MallocMetadataNode *current = this->head;
+        MallocMetadataNode *prev = current;
+        while (current != nullptr) {
+            current = current->next;
+            delete prev;
+            prev = current;
+        }
+    }
+
 };
+
+// TODO: remembe to add "delete mallocMetadataNodeList;"
+MallocMetadataNodeList* mallocMetadataNodeList= new MallocMetadataNodeList();
 
 
 void* smalloc(size_t size){
+    if(size<=0 || size>1e8) return NULL;
+    MallocMetadataNode* current = mallocMetadataNodeList->head->next;
 
+    while (current != nullptr){
+        if(current->is_free && current->size>size){
+            current->is_free = false;
+            mallocMetadataNodeList->numOfFreeBlocks--;
+            mallocMetadataNodeList->numOfUsedBlocks++;
+            mallocMetadataNodeList->sizeOfFreeBlocks-=current->size;
+            mallocMetadataNodeList->sizeOfUsedBlocks+=current->size;
+            return current->allocatedBlockPtr;
+        }
+        current = current->next;
+    }
+
+    void* previousBRK =  sbrk(size);
+    if(*((int*)previousBRK) == -1) return NULL;
+
+    MallocMetadataNode* newMetadataNode = new MallocMetadataNode(previousBRK,size);
+
+    mallocMetadataNodeList->tail->next = newMetadataNode;
+    newMetadataNode->prev=mallocMetadataNodeList->tail;
+    mallocMetadataNodeList->tail = newMetadataNode;
+
+    return previousBRK;
 }
 
 void* scalloc(size_t num, size_t size){
@@ -44,25 +95,28 @@ void* srealloc(void* oldp, size_t size){
 }
 
 size_t _num_free_blocks(){
-
+    return mallocMetadataNodeList->numOfFreeBlocks;
 }
 
 size_t _num_free_bytes(){
-
+    return mallocMetadataNodeList->sizeOfFreeBlocks;
 }
 
 size_t _num_allocated_blocks(){
+    size_t blocksTotalSizeWithoutMetadata = (mallocMetadataNodeList->sizeOfFreeBlocks + mallocMetadataNodeList->sizeOfUsedBlocks);
+    size_t metadataTotalSize = (mallocMetadataNodeList->numOfFreeBlocks + mallocMetadataNodeList->numOfUsedBlocks)*(mallocMetadataNodeList->sizeOfSingleMetadataBlock);
+    return blocksTotalSizeWithoutMetadata + metadataTotalSize;
 
 }
 
 size_t _num_allocated_bytes(){
-
+    return (mallocMetadataNodeList->sizeOfFreeBlocks + mallocMetadataNodeList->sizeOfUsedBlocks);
 }
 
 size_t _num_meta_data_bytes(){
-
+    return (mallocMetadataNodeList->numOfFreeBlocks + mallocMetadataNodeList->numOfUsedBlocks)*(mallocMetadataNodeList->sizeOfSingleMetadataBlock);
 }
 
 size_t _size_meta_data(){
-
+    return mallocMetadataNodeList->sizeOfSingleMetadataBlock;
 }
